de64a9654e2387281567e1c0c0128ba2c0eaba8a
Parvina Odinaeva <odinaeva.parvina@gmail.com>
2022-06-15 15:47:40 +0100

    functions js changed

--- js/script.js
+++ js/script.js
@@ -1,25 +1,34 @@
-console.log("Dzień dobry wszystkim!")
-
-let tlo = document.querySelector(".tlo");
-let body = document.querySelector(".body");
-let theme = document.querySelector(".theme");
-
-tlo.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    theme.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
-});
-
-
-let button = document.querySelector(".button");
-let note = document.querySelector(".note");
-let word = document.querySelector(".word");
-
-button.addEventListener("click", () => {
-    note.classList.toggle("hide");
-    if (note.classList.contains("hide")) {
-        word.innerText = "Pokaż:";
-    } else {
-        word.innerText = "Ukryj:";
+{
+    const welcome = () => {
+        console.log("Dzień dobry wszystkim!");
     }
-});
+    welcome();
 
+    const backgroundButtonClick = () => {
+        let body = document.querySelector(".body");
+        let theme = document.querySelector(".theme");
+        body.classList.toggle("dark");
+        theme.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+    }
+
+    const init = () => {
+        let tlo = document.querySelector(".tlo");
+        tlo.addEventListener("click", backgroundButtonClick);
+    }
+    init();
+}
+
+{
+    let button = document.querySelector(".button");
+    let note = document.querySelector(".note");
+    let word = document.querySelector(".word");
+
+    button.addEventListener("click", () => {
+        note.classList.toggle("hide");
+        if (note.classList.contains("hide")) {
+            word.innerText = "Pokaż:";
+        } else {
+            word.innerText = "Ukryj:";
+        }
+    });
+}
